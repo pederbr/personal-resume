72f1cdf271045d7dfe5d589e09324430822630bf
Peder Brennum <pedebr@stud.ntnu.no>
2025-03-24 09:22:55 +0100

    switch db provider

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,5 @@
 // storage-adapter-import-placeholder
-import { sqliteAdapter } from '@payloadcms/db-sqlite'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
 
 import sharp from 'sharp' // sharp-import
 import path from 'path'
@@ -58,10 +58,8 @@ export default buildConfig({
   },
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URI || '',
-    },
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URI || 'mongodb://localhost/payload-cms',
   }),
   collections: [Pages, Posts, Media, Categories, Users, WorkExperience],
   cors: [getServerSideURL()].filter(Boolean),
